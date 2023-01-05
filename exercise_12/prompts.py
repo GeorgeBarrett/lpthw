29d9086fded6ddf43c80db217a2047cf6cd88628
George Barrett <georgeaustinbarrett@gmail.com>
2023-01-05 15:43:38 +0000

    exercise 12 complete. prompting users and practicing f strings

---
+++ exercise_12/prompts.py
@@ -0,0 +1,5 @@
+favourite_fruit = input('What\'s your favourite fruit? ')
+favourite_band = input('What\'s your favourite band? ')
+favourite_dj = input('Who is your favourite dj? ')
+
+print(f'So {favourite_fruit} is your favourite fruit, {favourite_band} is your favourite band and {favourite_dj} is your favourite dj.')
