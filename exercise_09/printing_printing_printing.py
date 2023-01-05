458563192d399bcdc216a8e3323ec1de2967793d
George Barrett <georgeaustinbarrett@gmail.com>
2023-01-05 13:41:11 +0000

    exercise 9 complete. more more more printing printing printing

---
+++ exercise_09/printing_printing_printing.py
@@ -0,0 +1,12 @@
+days = 'Monday Tuesday Wednesday Thursday Friday etc'
+months = 'January\nFebruary\nMarch\nApril\nMay\netc'
+
+print('Here are the days: ', days)
+print('Here are the months: ', months)
+
+print('''
+    I can write whatever I want
+    and the console
+    will automatically recognise
+    my line breaks
+''')
