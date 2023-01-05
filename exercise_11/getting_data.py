0572bb54b480a85eb2bf93741347c614b3678536
George Barrett <georgeaustinbarrett@gmail.com>
2023-01-05 14:46:10 +0000

    exercise 11 complete. gathering and printing data

---
+++ exercise_11/getting_data.py
@@ -0,0 +1,10 @@
+print('How old are you?', end=' ')
+age = input()
+
+print('How tall are you?', end=' ')
+height = input()
+
+print('What colour are your eyes?', end=' ')
+colour = input()
+
+print('So, you are', age, 'years old,', height, 'tall and have', colour, 'eyes.')
