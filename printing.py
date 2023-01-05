6bae0983fbd167c3b0c422cd80556e1de8486a8a
George Barrett <georgeaustinbarrett@gmail.com>
2023-01-05 12:55:53 +0000

    exercise 7 complete. more printing and variable practice in python

---
+++ printing.py
@@ -0,0 +1,20 @@
+print('Mary had a little lamb.')
+print('It\'s fleece was as white as {}.'.format('snow'))
+print('And everywhere Mary went.')
+print('She listned to.')
+print('.' * 10)
+
+
+end1 = 'A'
+end2 = 'p'
+end3 = 'h'
+end4 = 'e'
+end5 = 'x'
+
+end6 = 'T'
+end7 = 'w'
+end8 = 'i'
+end9 = 'n'
+
+print(end1 + end2 + end3 + end4 + end5, end=' ')
+print(end6 + end7 + end8 + end9)
